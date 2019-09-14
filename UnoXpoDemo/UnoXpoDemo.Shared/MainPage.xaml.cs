5f281465299099d2cc86f8561afd107075768f56
Jose Javier Columbie <jose@shopcontroller.com>
2019-09-14 13:36:22 -0700

    Add project files.

---
+++ UnoXpoDemo/UnoXpoDemo.Shared/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnoXpoDemo.Shared.Model;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UnoXpoDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            XPCollection<Item> itemCollection = new XPCollection<Item>();
+            ObservableCollection<Item> Data = new ObservableCollection<Item>(itemCollection);
+            this.itemListView.ItemsSource = Data;
+        }
+    }
+}
---
+++ UnoXpoDemo/UnoXpoDemo.Shared/Model/Item.cs
@@ -0,0 +1,41 @@
+﻿using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnoXpoDemo.Shared.Model
+{
+    public class Item : XPObject
+    {
+        public Item()
+        {
+        }
+        public Item(Session session) : base(session)
+        {
+        }
+
+        string _name;
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Name
+        {
+            get => _name;
+            set => SetPropertyValue(nameof(Name), ref _name, value);
+        }
+
+        string description;
+        public string Description
+        {
+            get { return description; }
+            set { SetPropertyValue("Description", ref description, value); }
+        }
+
+        bool _active;
+        public bool Active
+        {
+            get => _active;
+            set => SetPropertyValue(nameof(Active), ref _active, value);
+        }
+
+
+    }
+}
